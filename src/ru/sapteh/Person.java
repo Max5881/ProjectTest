6917dc6774cafeb121cf22b988ce7a0329384f8c
Max <gsx401@yandex.ru>
2020-09-29 12:00:41 +0300

    Person

--- src/ru/sapteh/Person.java
+++ src/ru/sapteh/Person.java
@@ -3,8 +3,23 @@ package ru.sapteh;
 public class Person {
     private String name;
     private  int age;
+    private int date;
 
+    public Person(String name,int age, int date){
+        this.name = name;
+        this.age = age;
+        this.date = date;
+    }
+    public Person (String name, int age){
+        this.name=name;
+        this.age = age;
+    }
+    public Person (String name){
+        this.name=name;
+    }
+    public Person(){}
     public String getName(){
+
         return name;
     }
     public int getAge(){
@@ -15,5 +30,16 @@ public class Person {
     }
     public void setAge(int age){
         this.age = age;
+    }
+    public int getDate() {
+        return date;
+    }
+    public void setDate(int date){
+        this.date = date;
+    }
+    public String toString(){
+        return "Name: "+ getName()+ "\n" +
+         "Age: "+ getAge()+ "\n"+
+         "Date: "+ getDate()+ "\n";
     }
 }
--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -2,6 +2,11 @@ package ru.sapteh;
 
 public class Program {
     public static void main (String[] args){
-        System.out.println("Hello Max Poca");
+        Person person = new Person("Ivan",174,27);
+        Person person1 = new Person("Artem",19,4);
+        Person person2 = new Person("Matvyi",800,78);
+        System.out.println(person.toString());
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
     }
 }
